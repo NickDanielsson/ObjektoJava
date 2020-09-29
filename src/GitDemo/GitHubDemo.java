3862f34ead1907fd73a534346224bb4d1908ec6d
Nick Danielsson <nickdanielsson87@gmail.com>
2020-09-29 11:43:15 +0200

    Initial commit

---
+++ src/GitDemo/GitHubDemo.java
@@ -0,0 +1,13 @@
+package GitDemo;
+
+/**
+ * Created by Nick Danielsson
+ * Date 2020-09-29
+ * Time 11:41
+ * Project ObjektoJava
+ */
+public class GitHubDemo {
+    public static void main(String[] args) {
+        System.out.println("HEEEEEJ");
+    }
+}
