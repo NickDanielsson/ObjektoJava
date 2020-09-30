acbf2a9516ef595c56383fdca8a9fa5009a91c3a
Nick Danielsson <nickdanielsson87@gmail.com>
2020-09-30 11:06:42 +0200

    Inlupp 1 Snake

---
+++ src/Inlupp1/Snake.java
@@ -0,0 +1,24 @@
+package Inlupp1;
+
+import javax.swing.*;
+
+/**
+ * Created by Nick Danielsson
+ * Date 2020-09-28
+ * Time 11:01
+ * Project ObjektoJava
+ */
+public class Snake extends Animal{
+
+    //enum
+    Animalfood snakefood = Animalfood.ORMPELLETS;
+
+    public Snake(int weight, String name) {
+        super(weight, name);
+    }
+
+    @Override
+    public void getFood() {
+        JOptionPane.showMessageDialog(null,20 + " gram " + snakefood);
+    }
+}
